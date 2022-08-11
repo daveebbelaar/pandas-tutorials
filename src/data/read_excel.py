72e260f3e7f97d14a491c3aadbc44bdd056944f1
Dave Ebbelaar <dave.ebbelaar@gmail.com>
2022-08-11 12:12:43 +0200

    project setup

--- src/data/read_excel.py
+++ src/data/read_excel.py
@@ -1,1 +1,11 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plot_settings
+
+
+data = pd.read_excel(
+    "../../data/raw/pump_sensor_data/20180401_pump_sensor_data.xlsx",
+    parse_dates=[0],
+    index_col=[0],
+)
